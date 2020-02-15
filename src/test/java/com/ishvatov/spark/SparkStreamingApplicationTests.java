18c7a34f625da00a08ad5578f2b449ccb739909e
ishvatov <ishvatov@gmail.com>
2020-02-15 18:36:15 +0300

    Commit by Sergey Hvatov on 15.02.20: - created basic spring console application and added all dependencies that are necessary at the moment - added application.yml - added logback-spring.xml

---
+++ src/test/java/com/ishvatov/spark/SparkStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishvatov.spark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
